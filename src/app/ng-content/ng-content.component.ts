5704506157e10491495a54e45e582418ebce9f96
wangzixi-diablo <cle.ee@163.com>
2022-03-22 09:11:39 +0800

    nested form

--- src/app/ng-content/ng-content.component.ts
+++ src/app/ng-content/ng-content.component.ts
@@ -77,6 +77,10 @@ export class NameEditorComponent {
   profileForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+    })
   });
   
   constructor(){
